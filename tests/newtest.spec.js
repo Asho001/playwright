3a0f5f33df7a31635acb595507978865c9b56275
Ashok Kumar M <AshokKumarM@teamblr.com>
2024-05-29 09:52:27 +0530

    salary cal

--- tests/newtest.spec.js
+++ tests/newtest.spec.js
@@ -5,14 +5,23 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Login ID').fill('T4297');
   await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill('18@december');
-  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.click("//input[@id='btnsignin']");
+  await page.goto('https://alcsaws.teamlease.com/GUI/Login/Home.aspx');
   await page.getByRole('link', { name: 'menu' }).click();
-  await page.getByRole('link', { name: 'keyboard_arrow_down account_circle Master' }).click();
-  await page.getByRole('button', { name: 'Regulatory Setup' }).click();
-  await page.getByRole('link', { name: 'Commercial Setup', exact: true }).click();
-  await page.getByRole('cell', { name: 'Client' }).locator('label').click();
-  await page.getByTitle('-- Select Client --').click();
-  await page.getByRole('treeitem', { name: '05NDL / Gillette India Ltd...' }).click();
+  await page.getByRole('link', { name: 'keyboard_arrow_down view_carousel Inputs' }).click();
+  await page.getByRole('button', { name: 'Associate Master' }).click();
+  await page.getByRole('link', { name: 'Salary Calculator', exact: true }).click();
+  await page.getByTitle('--Select Client--').click();
+  await page.getByRole('treeitem', { name: '00VMB \\ BASF India Limited' }).click();
+  await page.locator('#select2-AlcsContentPlaceHolder_ddlState-container').click();
+  await page.getByRole('treeitem', { name: 'Goa' }).click();
+  await page.locator('#select2-AlcsContentPlaceHolder_ddlIndustryType-container').click();
   
-  await page.close()
+  await page.getByRole('treeitem', { name: 'Pharmaceutical Industry' }).click();
+  await page.getByRole('treeitem', { name: 'Aldana' }).click();
+  await page.goto('http://alcsaws.teamlease.com//GUI/Associate/SalaryCalculator.aspx');
+  await page.getByLabel('* Amount :').click();
+  await page.getByLabel('* Amount :').fill('18000');
+  // await page.goto('http://alcsaws.teamlease.com//GUI/Associate/SalaryCalculator.aspx');
+  await page.pause();
 });
